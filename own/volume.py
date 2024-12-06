7b24ac1bf434a0ff8a9180d75cbb9431c1ad18b7
aegroto <aegroto@protonmail.com>
2024-12-06 12:59:59 +0100

    added volume and fade self implementation

--- own/volume.py
+++ own/volume.py
@@ -4,10 +4,9 @@ from typing import List
 from own.io import read_samples, write_samples
 
 
-def change_volume(
-    original: List[int], factor: float
-) -> List[int]:
+def change_volume(original: List[int], factor: float) -> List[int]:
     return [sample * factor for sample in original]
+
 
 def main():
     parser = ArgumentParser()
@@ -19,6 +18,7 @@ def main():
     samples, sample_rate = read_samples(args.input)
     volume_changed = change_volume(samples, args.factor)
     write_samples(volume_changed, sample_rate, args.output)
+
 
 if __name__ == "__main__":
     main()
